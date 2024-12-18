f36f5d68fb2cfa813d5d9fae5633247768f1a1ab
PapSara <114569211+PapSara@users.noreply.github.com>
2024-12-18 11:43:54 +0200

    Adaugare coordonate pe linie si coloana
    1,2,3 pe linii si A, B, C pe coloane

--- proiectso2.c
+++ proiectso2.c
@@ -2,22 +2,22 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
-//definim global caracterele din tabela extinsa ASCII de care vom avea nevoie pentru a desena patratul
-char colt_stanga_sus = 218;    // ┌
-char colt_dreapta_sus = 191;   // ┐
+// definim caracterele din tabela extinsa ASCII de care vom avea nevoie pentru a desena patratul
+char colt_stanga_sus = 218;    // ┐
+char colt_dreapta_sus = 191;   // ┒
 char colt_stanga_jos = 192;    // └
 char colt_dreapta_jos = 217;   // ┘
 char orizontala = 196;         // ─
 char verticala = 179;          // │
 char separator_sus = 194;      // ┬
 char separator_jos = 193;      // ┴
-char separator_stanga = 195;  // ├
+char separator_stanga = 195;   // ├
 char separator_central = 197;  // ┼
 char separator_dreapta = 180;  // ┤
 
 // tabla initial goala de 3x3 sub forma de matrice pentru a putea plasa X sau 0 
-
 char tabla[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
 
 void print_orizontala() // functie care va printa o linie orizontala corespunzatoare unei singure casute din patrat
@@ -27,7 +27,8 @@ void print_orizontala() // functie care va printa o linie orizontala corespunzat
 
 void print_linie_sus() // functie care printeaza latura de sus a patratului cu colturi si delimitari intre primele 3 casute 
 {
-    printf("%c", colt_stanga_sus);
+    printf("    A     B     C\n"); // Adaugam literele pentru coloane
+    printf("  %c", colt_stanga_sus);
     print_orizontala();
     printf("%c", separator_sus);
     print_orizontala();
@@ -38,7 +39,7 @@ void print_linie_sus() // functie care printeaza latura de sus a patratului cu c
 
 void print_linie_mijloc() // functie care printeaza latura de mijloc a patratului cu colturi si delimitari intre cele 3 casute 
 {
-    printf("%c", separator_stanga);
+    printf("  %c", separator_stanga);
     print_orizontala();
     printf("%c", separator_central);
     print_orizontala();
@@ -49,7 +50,7 @@ void print_linie_mijloc() // functie care printeaza latura de mijloc a patratulu
 
 void print_linie_jos() // functie care printeaza latura de jos a patratului cu colturi si delimitari intre ultimele 3 casute 
 {
-    printf("%c", colt_stanga_jos);
+    printf("  %c", colt_stanga_jos);
     print_orizontala();
     printf("%c", separator_jos);
     print_orizontala();
@@ -60,7 +61,7 @@ void print_linie_jos() // functie care printeaza latura de jos a patratului cu c
 
 void print_celule(int rand) // functie care printeaza laturile verticale ale patratului la distanta de o "casuta" si primeste ca parametru un intreg reprezentand numarul randului din tabla
 {
-    printf("%c  %c  %c  %c  %c  %c  %c\n", verticala, tabla[rand][0], verticala, tabla[rand][1], verticala, tabla[rand][2], verticala);
+    printf("%d %c  %c  %c  %c  %c  %c  %c\n", rand + 1, verticala, tabla[rand][0], verticala, tabla[rand][1], verticala, tabla[rand][2], verticala);
 }
 
 void print_tabla() 
@@ -124,57 +125,54 @@ void reseteaza_tabla()
     }
 }
 
+// Conversia coordonatelor (ex. "A1") in indicii matricei
+int coordonate_la_indici(char* coordonate, int* rand, int* coloana) 
+{
+    if(strlen(coordonate) != 2) 
+    {
+        return 0; // coordonate invalide
+    }
+    
+    char litera = toupper(coordonate[0]); // transformam litera in majuscula
+    char cifra = coordonate[1];
+
+    if(litera < 'A' || litera > 'C' || cifra < '1' || cifra > '3') 
+    {
+        return 0; // coordonate invalide
+    }
+
+    *rand = cifra - '1'; // convertim '1', '2', '3' in 0, 1, 2
+    *coloana = litera - 'A'; // convertim 'A', 'B', 'C' in 0, 1, 2
+
+    return 1; // coordonate invalide
+}
+
 void scrie_X_sau_0() 
 {
     int flag = 0; // 0 pentru X, 1 pentru 0
     char caracter;
     int rand, coloana;
     int mutari; 
+    char coordonate[3];
     char optiune[3];
-    char player1[100], player2[100];
-    char simbol1, simbol2;
+    char jucator1[50], jucator2[50];
 
-    printf("Player 1, ce username ai vrea sa ai? ");
-    scanf("%s", player1);
-    printf("Player 1 are username-ul: %s", player1);
+    printf("Introduceti numele jucatorului pentru X: ");
+    scanf("%s", jucator1);
+    printf("Introduceti numele jucatorului pentru 0: ");
+    scanf("%s", jucator2);
 
     while(1)
     {
-        while (1) 
-        {
-            printf("%s, alege simbolul cu care ai vrea sa joci (X sau 0): ", player1);
-            scanf(" %c", &simbol1);
-
-            if((simbol1 == 'X') || (simbol1 == 'x')) 
-            {
-                simbol1 = 'X';
-                simbol2 = '0';
-                break;
-            } 
-            else if(simbol1 == '0') 
-            {
-                simbol2 = 'X';
-                break;
-            } 
-            else 
-            {
-                printf("%s, ai introdus un simbol invalid. Incearca din nou.\n", player1);
-            }
-        }
-
-        printf("Player 2, ce username ai vrea sa ai? ");
-        scanf("%s", player2);
-        printf("Player 2 are username-ul: %s", player2);
-
-        printf("%s va juca %c.\n", player1, simbol1);
-        printf("%s va juca %c.\n", player2, simbol2);
+        caracter = 'X';
+        flag = 0; // Resetam la jucatorul X la inceputul fiecarui joc
 
         for(mutari = 0; mutari < 9; mutari++) 
         { 
-            printf("Introdu pozitia pentru %c (rand coloana): ", caracter);
-            scanf("%d %d", &rand, &coloana);
+            printf("%s, introdu pozitia pentru %c (ex: A1): ", flag == 0 ? jucator1 : jucator2, caracter);
+            scanf("%s", coordonate);
 
-            if(rand < 0 || rand > 2 || coloana < 0 || coloana > 2) 
+            if(!coordonate_la_indici(coordonate, &rand, &coloana)) 
             {
                 printf("S-a introdus o pozitie invalida. Incearca din nou.\n");
                 mutari--; 
@@ -194,30 +192,22 @@ void scrie_X_sau_0()
 
             if(verificare_castig()) 
             {
-                printf("Winner winner chicken dinner: %c\n", caracter);
+                printf("Felicitari, %s! Ai castigat!\n", flag == 0 ? jucator1 : jucator2);
                 break;
             }
 
-            if(flag == 0)
-            {
-                caracter = '0';
-            }
-            else
-            {
-                caracter = 'X';
-            }
-
+            caracter = (caracter == 'X') ? '0' : 'X';
             flag = 1 - flag; 
         }
 
-        if(verificare_castig() == 0)
+        if(verificare_castig() == 0 && mutari == 9)
         {
             printf("Remiza.\n");
         }
 
         reseteaza_tabla();
-        printf("Jucati din nou? Introduceti Da sau Nu.\n");
-        scanf(" %s", &optiune);
+        printf("Jucati din nou? Introduceti Da sau Nu: ");
+        scanf("%s", optiune);
 
         if(strcmp(optiune, "Da") != 0)
         {
@@ -229,12 +219,9 @@ void scrie_X_sau_0()
     }
 }
 
-
-int main(int argc, char* argv[]) 
+int main() 
 {
     print_tabla();
-
     scrie_X_sau_0();
     return 0;
 }
-
